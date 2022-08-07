6f28e91d0e5f137a390c986c3ef802115bece926
shake <hiroki.yamako@ezweb.ne.jp>
2022-08-07 18:25:52 +0900

    first commit

---
+++ setting.py
@@ -0,0 +1,8 @@
+import pykintone
+
+# kintone APIの設定（全処理共通）
+subdomein = "ajrl2h58jbtu"
+appId = "1"
+token = "1mdpHFfPZXm2BEDIqOeKtdgb9H4SJHpEvzKbTZE6"
+app = pykintone.app(subdomein, appId, token)
+
